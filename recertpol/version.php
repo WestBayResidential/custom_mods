3ac378ef6e8056141cf89f7a35aefa67e7ac4c94
Paul LaRiviere <plariv@augurynet.com>
2014-10-10 19:24:32 +0000

    Fix version issue for phpunit init

--- recertpol/version.php
+++ recertpol/version.php
@@ -29,8 +29,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-//$module->version   = 2014082800;      // If version == 0 then module will not be installed
-$module->version   = 0;      // If version == 0 then module will not be installed
+$module->version   = 2014082800;      // If version == 0 then module will not be installed
+//$module->version   = 0;      // If version == 0 then module will not be installed
 //$module->version   = 2010032200;      // The current module version (Date: YYYYMMDDXX)
 $module->requires  = 2010031900;      // Requires this Moodle version
 $module->cron      = 0;               // Period for cron to check this module (secs)
