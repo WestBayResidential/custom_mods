42b2570bc3523efda1f185fbcb106a6875c22fa1
Paul LaRiviere <plariv@augurynet.com>
2015-04-02 14:41:15 -0400

    Use fully qualified pathname to jquery plugin

--- staff/jquery/plugins.php
+++ staff/jquery/plugins.php
@@ -1,8 +1,8 @@
 <?php
 
 $plugins = array(
-    'staff-jquerymodule' => array( 'files' => array( 'jquerymodule/media/js/jquery.dataTables.js'),
-    'staff-jquerymodule-css' => array( 'files' => array( 'jquerymodule/media/css/jquery.dataTables.css'))
+    'staff-jquerymodule' => array( 'files' => array( '/var/www/moodle/enrol/staff/jquery/jquerymodule/media/js/jquery.dataTables.js'),
+    'staff-jquerymodule-css' => array( 'files' => array( '/var/www/moodle/enrol/staff/jquery/jquerymodule/media/css/jquery.dataTables.css'))
  );
 
 ?>
