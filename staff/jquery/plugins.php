6bdcb2aa5553aa55ccf8e5b40958a63faa3c13fb
Paul LaRiviere <plariv@augurynet.com>
2015-04-02 16:17:57 -0400

    Using archaius as a model, refactored plugins spec

--- staff/jquery/plugins.php
+++ staff/jquery/plugins.php
@@ -1,8 +1,10 @@
 <?php
 
 $plugins = array(
-    'staff-jquerymodule' => array( 'files' => array( 'enrol/staff/jquery/jquerymodule/media/js/jquery.dataTables.js'),
-    'staff-jquerymodule-css' => array( 'files' => array( 'enrol/staff/jquery/jquerymodule/media/css/jquery.dataTables.css'))
+  'staff-jquerymodule' => array( 
+    'files' => array( 'source/dataTables-1.10.5.js')
+  ),
+  'staff-jquerymodule-css' => array( 
+    'files' => array( 'source/dataTables-1.10.5.css')
+  )
  );
-
-?>
