1fa7b2dc39ce04e57c59bf3ad8081b9b639e93f6
Paul LaRiviere <plariv@augurynet.com>
2015-04-02 16:47:29 -0400

    Missing component designation

--- staff/jquery/plugins.php
+++ staff/jquery/plugins.php
@@ -2,9 +2,9 @@
 
 $plugins = array(
   'staff-jquerymodule' => array( 
-    'files' => array( 'source/dataTables-1.10.5.js')
+    'files' => array( 'source/dataTables-1.10.5.js', 'enrol_staff')
   ),
   'staff-jquerymodule-css' => array( 
-    'files' => array( 'source/dataTables-1.10.5.css')
+    'files' => array( 'source/dataTables-1.10.5.css', 'enrol_staff')
   )
  );
