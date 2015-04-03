f8ef50adb097cf1bcfe50d8d96fdc40d75b19d02
Paul LaRiviere <plariv@augurynet.com>
2015-04-02 20:36:32 -0400

    Put component designation in the correct function

--- staff/jquery/plugins.php
+++ staff/jquery/plugins.php
@@ -1,10 +1,10 @@
 <?php
 
 $plugins = array(
-  'staff-jquerymodule' => array( 
-    'files' => array( 'source/dataTables-1.10.5.js', 'enrol_staff')
+  'staff-datatable' => array( 
+    'files' => array( 'source/dataTables-1.10.5.js')
   ),
-  'staff-jquerymodule-css' => array( 
-    'files' => array( 'source/dataTables-1.10.5.css', 'enrol_staff')
+  'staff-datatable-css' => array( 
+    'files' => array( 'source/dataTables-1.10.5.css')
   )
  );
