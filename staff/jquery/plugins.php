5634331d4dd2026d8f79deac446778fc0d751da4
Paul LaRiviere <plariv@augurynet.com>
2018-03-07 09:23:50 -0500

    Updated plugins arrays for jquery loading

--- staff/jquery/plugins.php
+++ staff/jquery/plugins.php
@@ -2,9 +2,15 @@
 
 $plugins = array(
   'staff-datatable' => array( 
-    'files' => array( 'source/dataTables-1.10.5.js','source/dataTables-checkboxes.min.js')
+    'files' => array( 'source/dataTables-1.10.5.js')
   ),
   'staff-datatable-css' => array( 
-    'files' => array( 'source/dataTables-1.10.5.css','source/dataTables-checkboxes.css')
+    'files' => array( 'source/dataTables-1.10.5.css')
+  ),
+  'staff-datatable-checkboxes' => array( 
+    'files' => array( 'source/dataTables-checkboxes.js')
+  ),
+  'staff-datatable-checkboxes-css' => array( 
+    'files' => array( 'source/dataTables-checkboxes.css')
   )
 );
