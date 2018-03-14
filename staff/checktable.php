bf6c3684f040badfa60572ae21e11beaf890e477
Paul LaRiviere <plariv@augurynet.com>
2018-03-13 23:01:46 -0400

    Changed checkbox option in setup; removed quotes around class

--- staff/checktable.php
+++ staff/checktable.php
@@ -27,7 +27,7 @@ function build_checktable($courselist, $namelist)
     $cnum = $cnum ? $cnum : "NA";
     $cnumlist[ $corder ] = $cnum;
     $checktable .= "<th class=\"dt-head-center\"> {$cname} <br />";
-    $checktable .= "<input type=\"checkbox\" name=\"col_{$corder}\" \"class\"=\"th-checkit\" /></th>";
+    $checktable .= "<input type=\"checkbox\" name=\"col_{$corder}\" /></th>";
   }
   $checktable .= '</tr></thead><tbody>';
   
