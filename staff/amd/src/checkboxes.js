3a4983de593a4b1d2e3f20a78de7863143c1973b
Paul LaRiviere <plariv@augurynet.com>
2018-11-06 23:05:38 -0500

    Fix path to checkboxes.js plugin

---
+++ staff/amd/src/checkboxes.js
@@ -0,0 +1,5 @@
+define(['enrol_staff/config', 'checkboxes'], function(unused,checkboxes) {
+      return checkboxes;
+   }
+);
+
--- staff/holding/checkboxes.js
+++
@@ -1,5 +0,0 @@
-define(['mod_staff/config', 'checkboxes'], function(unused,checkboxes) {
-      return checkboxes;
-   }
-);
-
