af19fa9282d2b75c5299e26e9b9e3c9fff0692dd
Paul LaRiviere <plariv@augurynet.com>
2018-11-12 09:32:10 -0500

    Renamed definition files to shim names, and corrected config

--- staff/amd/src/config.js
+++ staff/amd/src/config.js
@@ -2,12 +2,12 @@ define([], function () {
     window.requirejs.config({
         paths: {
            //Enter the paths to your required java-script files  
-           "datatables.net": M.cfg.wwwroot + '/enrol/staff/js/DataTables-1.10.18/js/jquery.dataTables.min.js',
-           "datatables.net-select": M.cfg.wwwroot + '/enrol/staff/js/Select-1.2.6/js/select.dataTables.js'
+           "datatables.net": M.cfg.wwwroot + '/enrol/staff/js/DataTables-1.10.18/js/jquery.dataTables.min',
+           "datatables.net-select": M.cfg.wwwroot + '/enrol/staff/js/Select-1.2.6/js/select.dataTables'
         },
         shim: {
            //Enter the "names" that will be used to refer to your libraries
-           'datatables.net': {exports: 'datatables.net'},
+           'datatables.net': {exports: 'dataTables'},
            'datatables.net-select': {exports: 'select'}
         }
     });
